80c91907e442c52c5065eb9b7f2470560ec8c004
cristRodsz. <cristianrodriguesouza@hotmail.com>
2024-02-09 03:32:03 -0300

    first commit

---
+++ src/main/java/com/tabnews/tabnews/TabnewsApplication.java
@@ -0,0 +1,13 @@
+package com.tabnews.tabnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabnewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabnewsApplication.class, args);
+	}
+
+}
